dc0abb91bc34ccdd5ae880b5b83f16d4de26c5cf
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-06-22 13:33:58 -0300

    feat: Testes da class Ingredient_Req_1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,25 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, restriction_map  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_one = Ingredient("frango")
+    ingredient_two = Ingredient("camarão")
+    ingredient_three = Ingredient("frango")
+
+    assert ingredient_one.name == "frango"
+
+    # Retorna hashes iguais
+    assert hash(ingredient_one) == hash(ingredient_three)
+
+    # Retorna hashes diferentes
+    assert hash(ingredient_one) != hash(ingredient_two)
+
+    assert ingredient_one == ingredient_three
+
+    assert ingredient_two != ingredient_three
+
+    expected_restrictions = restriction_map().get("camarão", set())
+    assert ingredient_two.restrictions == expected_restrictions
+
+    assert ingredient_three.__repr__() == "Ingredient('frango')"
