1dc422418bfa7e70cd621abd33a8d4eb0131621f
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-06-23 15:53:08 -0300

    feat: criação da class MenuData_Req_3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,28 @@
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import csv
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self._dishes = set()
+
+        with open(source_path, "r") as csv_file:
+            file = csv.DictReader(csv_file)
+
+            for row in file:
+                dish = row["dish"]
+                price = float(row["price"])
+                ingredient = row["ingredient"]
+                recipe_amount = int(row["recipe_amount"])
+
+                #  Cria um objeto com o nome e o preço
+                self.dishes.add(Dish(dish, price))
+
+                # o iterador é criado internamente no conjunto self.dishes
+                dish = self.dishes
+
+                dish.add_ingredient_dependency(
+                    Ingredient(ingredient), recipe_amount
+                )
